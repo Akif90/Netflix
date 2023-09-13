184a0719c11a91c883a8ece4018127e82702c94c
Akif Ahmed <akifahmed90@gmail.com>
2023-09-13 21:38:47 +0530

    Created a Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import {signOut} from "next-auth/react";
 import {useSession} from "next-auth/react";
 import {redirect} from "next/navigation";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   const {data: session} = useSession({
@@ -13,18 +14,7 @@ export default function Home() {
   });
   return (
     <>
-      <h1 className="text-white text-lg text-center my-5">
-        Welcome to Netflix
-      </h1>
-      <h2 className="text-white text-lg text-center my-5">
-        Logged in as: {session?.user?.name}
-      </h2>
-      <button
-        className="w-full rounded-xl bg-slate-500 my-5 hover:bg-neutral-800 h-10 text-white"
-        onClick={() => signOut()}
-      >
-        Sign out
-      </button>
+      <Navbar />
     </>
   );
 }
