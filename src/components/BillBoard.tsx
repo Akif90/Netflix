d69b2ddb875434b3c0a7ab07378ca6ba3cf8d191
Akif Ahmed <akifahmed90@gmail.com>
2023-09-14 20:23:52 +0530

    Created BillBoard component

--- src/components/BillBoard.tsx
+++ src/components/BillBoard.tsx
@@ -1,7 +1,39 @@
+import useBillBoard from "@/hooks/useBillBoard";
 import React from "react";
-
+import {AiOutlineInfoCircle} from "react-icons/ai";
 const BillBoard = () => {
-  return <div>BillBoard</div>;
+  const {data} = useBillBoard();
+  return (
+    <div className="relative h-[56.25vw] ">
+      <video
+        className="w-full h-[56.25vw] object-cover brightness-[60%]"
+        poster={data?.thumbnailUrl}
+        autoPlay
+        muted
+        loop
+        src={data?.videoUrl}
+      ></video>
+      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+        <p className="text-white text-1xl md:text-5xl h-full w-[50%] lg:text-6xl font-bold drop-shadow-xl">
+          {data?.title}
+        </p>
+        <p className="text-white text-[8px] md:text-lg mt-3 md:mt-8 w-[90%] md:w-[80%] lg:w-[50%] drop-shadow-xl ">
+          {data?.description}
+        </p>
+        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3 ">
+          <button
+            className="text-white bg-white rounded-md bg-opacity-30 py-1
+           md:py-2 px-2 md:px-4 w-auto
+            text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20
+            transition  "
+          >
+            More Info
+            <AiOutlineInfoCircle className="ml-1" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BillBoard;
